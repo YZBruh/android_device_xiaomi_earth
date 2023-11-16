b33be6420eae2ed213e52524f9cecc535c5df446
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-16 19:04:09 +0300

    Licenses

--- Android.mk
+++ Android.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),earth)
