0c097649b1cf466ad236185441e681bc7d661f5a
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-11-16 20:51:19 +0300

    Try fix/data mount error

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,9 +132,10 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TW_INCLUDE_FASTBOOTD := true
 TW_Y_OFFSET := 60
 TW_H_OFFSET := -60
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 
 # DEVICE
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 500
 TW_DEFAULT_BRIGHTNESS := 90
 TW_HAS_NO_RECOVERY_PARTITION := true
